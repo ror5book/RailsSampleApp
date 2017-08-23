5acce49cdf0912e100af48bb668694a5c7552011
ror5book <ror5@shoeisha.co.jp>
2017-08-23 16:16:02 +0900

    アプリケーションの基本設定

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,13 @@ module RailsSampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.skip_routes true
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
