bffbc98d6c78d09b7b2ffe9d78cb3a8cc9c69f6c
ror5book <ror5@shoeisha.co.jp>
2017-08-23 16:43:47 +0900

    bootstrapの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require turbolinks
+//= require tether
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
