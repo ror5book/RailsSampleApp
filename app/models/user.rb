3f90dd85734057431b5b659b964ffe52fbca7b9e
ror5book <ror5@shoeisha.co.jp>
2017-08-23 17:47:45 +0900

    Devise にユーザー名でもログインできる仕組みを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,14 +24,45 @@
 #  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string(255)
 #
 
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  validate :validate_name
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
+  attr_accessor :login
+
+  attr_writer :login
+
+  def login
+    @login || name || email
+  end
+
+  def validate_name
+    errors.add(:name, :invalid) if User.where(email: name).exists?
+  end
+
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email]&.downcase!
+    login = conditions.delete(:login)
+
+    where(conditions.to_hash).where(
+      ['lower(name) = :value OR lower(email) = :value',
+       { value: login.downcase }]
+    ).first
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823075629) do
+ActiveRecord::Schema.define(version: 20170823084038) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 20170823075629) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
