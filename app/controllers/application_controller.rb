21fa718b654f1fe53b1662c3f5c6ad12b910f1f7
ror5book <ror5@shoeisha.co.jp>
2017-08-24 06:11:20 +0900

    Active Adminの閲覧制限追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from SecurityError do |_exception|
+    redirect_to root_url, notice: 'アドミン画面へのアクセス権限がありません。'
+  end
+
   protected
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
+  end
 
   def configure_permitted_parameters
     added_attrs = %i[name email password password_confirmation remember_me avatar]
