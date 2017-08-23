3d4fad8bd9732c9c2d8695b1dbf4b0c21d8dd990
ror5book <ror5@shoeisha.co.jp>
2017-08-23 16:47:19 +0900

    トップページの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  APP_NAME = 'Rails Sample App'
+
+  def page_title
+    base_title = APP_NAME
+    return base_title if @title.blank?
+    "#{base_title} | #{@title}"
+  end
 end
