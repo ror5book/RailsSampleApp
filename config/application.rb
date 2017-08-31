73e303261d1a39054984e5733eb94b30b2c31b4b
ror5book <ror5@shoeisha.co.jp>
2017-09-01 08:39:33 +0900

    Encrypted secretsのセットアップ

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module RailsSampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.read_encrypted_secrets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
